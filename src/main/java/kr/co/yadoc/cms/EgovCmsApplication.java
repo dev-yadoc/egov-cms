49c665534a2fdcc366bce79d6d5576257d508bd2
dev-yadoc <35721182+dev-yadoc@users.noreply.github.com>
2026-08-14 14:40:02 +0900

    Add CMS application entry point

---
+++ src/main/java/kr/co/yadoc/cms/EgovCmsApplication.java
@@ -0,0 +1,11 @@
+package kr.co.yadoc.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgovCmsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EgovCmsApplication.class, args);
+    }
+}
